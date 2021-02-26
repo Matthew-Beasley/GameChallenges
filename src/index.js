d3c2c590a850352475f18b2b43ebf78b6aedf1f3
Matthew Beasley <matthewbeasley@Matthews-MacBook-Pro.local>
2021-02-26 12:40:10 -0800

    Added custom font

--- src/index.js
+++ src/index.js
@@ -4,6 +4,20 @@ import { RecoilRoot } from 'recoil';
 import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
 
+import '../assets/HKGrotesk2.41/TTF/HKGrotesk-Black.ttf'; 
+import '../assets/HKGrotesk2.41/TTF/HKGrotesk-Bold.ttf'; 
+import '../assets/HKGrotesk2.41/TTF/HKGrotesk-BoldItalic.ttf'; 
+import '../assets/HKGrotesk2.41/TTF/HKGrotesk-ExtraBold.ttf'; 
+import '../assets/HKGrotesk2.41/TTF/HKGrotesk-Italic.ttf';
+import '../assets/HKGrotesk2.41/TTF/HKGrotesk-Light.ttf';
+import '../assets/HKGrotesk2.41/TTF/HKGrotesk-LightItalic.ttf';
+import '../assets/HKGrotesk2.41/TTF/HKGrotesk-Medium.ttf';
+import '../assets/HKGrotesk2.41/TTF/HKGrotesk-MediumItalic.ttf';
+import '../assets/HKGrotesk2.41/TTF/HKGrotesk-Regular.ttf';
+import '../assets/HKGrotesk2.41/TTF/HKGrotesk-SemiBold.ttf';
+import '../assets/HKGrotesk2.41/TTF/HKGrotesk-SemiBoldItalic.ttf';
+
+
 const root = document.querySelector('#root');
 
 ReactDOM.render(
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,18 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
