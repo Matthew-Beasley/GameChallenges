1c25ffbdfb02471f41dff337d9f04a5d8a57c1a4
Matthew Beasley <matthewbeasley@Matthews-MacBook-Pro.local>
2021-03-06 20:22:34 -0800

    modal working

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,11 +1,19 @@
 import React, {useState, useEffect} from 'react';
 import { Route, Link, history } from 'react-router-dom';
 import { RecoilState, useRecoilState } from 'recoil';
+import Modal from 'react-modal';
 import { playerListState } from './RecoilState';
+
+Modal.setAppElement('#root');
 
 const NavBar = () => {
   const [initials, setInitials] = useState([]);
   const [players, setPlayers] = useRecoilState(playerListState);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const  toggleModal = () => {
+    setIsOpen(!isOpen);
+  };
 
   useEffect(() => {
     const tempArray = [];
@@ -24,8 +32,28 @@ const NavBar = () => {
         <Link to="/contact">Contact</Link>
       </div>
       <div id="dot-list">
-        {initials.map((letter, idx) => {return ( <div key={idx} className="player-dot"><div className="player-initial">{letter}</div></div> )})}
+        {initials.map((letter, idx) => {return ( 
+          <div 
+            key={idx} 
+            className="player-dot"
+            onClick={toggleModal}
+          >
+            <div 
+              className="player-initial">{letter}
+            </div>
+          </div> );})}
       </div>
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={toggleModal}
+        overlayClassName="myoverlay"
+        contentLabel="My dialog"
+        className="mymodal"
+        closeTimeoutMS={500}
+      >
+        <div>My modal dialog.</div>
+        <button onClick={toggleModal}>Close modal</button>
+      </Modal>
     </div>
   );
 };
