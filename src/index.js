39b3309340a069ac5be846cec295313b730a0487
Matthew Beasley <matthewbeasley@Matthews-MacBook-Pro.local>
2021-04-27 22:22:48 -0700

    moved landingpage styles to seperate file

--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ import 'semantic-ui-css/semantic.min.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import '../assets/styles/styles.css';
+import '../assets/styles/landingpage.css';
 
 const root = document.querySelector('#root');
 
