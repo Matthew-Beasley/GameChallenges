028946383b8fbb49b006be423e247c7d1e986929
Matthew Beasley <matthewbeasley@Matthews-MacBook-Pro.local>
2021-05-11 15:34:21 -0700

    added foxy endpoint

--- api/foxy.js
+++ api/foxy.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const foxyRouter = express.Router();
 
-foxyRouter.post((req, res, next) => {
-
+foxyRouter.post('/', async (req, res, next) => {
+  try {
+    console.log(req.body);
+    res.json({reply: 'hello back'});
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = foxyRouter;
