d265b4311a428b85d730f9f30c8e620f0cb2f107
Matthew Beasley <matthewbeasley@Matthews-MacBook-Pro.local>
2021-05-13 12:48:47 -0700

    foxy web hook connected

--- api/foxy.js
+++ api/foxy.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const foxyRouter = express.Router();
+const crypto = require('crypto');
+const foxyEncryptionKey = process.env.FOXY_ENCRYPTION_KEY;
+const foxyStoreId = process.env.FOXY_STORE_ID;
+const foxyStoreDomain = process.env.FOXY_STORE_DOMAIN;
 
 foxyRouter.post('/', async (req, res, next) => {
   try {
+    console.log(req.headers);
     console.log(req.body);
     res.json({reply: 'hello back'});
   } catch (err) {
