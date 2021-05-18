691eb80bd8ae5d1955190909b50dbc7f88d4a4db
Matthew Beasley <matthewbeasley@Matthews-MacBook-Pro.local>
2021-05-18 11:18:28 -0700

    testing header content

--- api/foxy.js
+++ api/foxy.js
@@ -2,7 +2,7 @@ const express = require('express');
 const foxyRouter = express.Router();
 const crypto = require('crypto');
 const { request } = require('http');
-const { addDeck } = require('../mongo/crud/users')
+const { addDeck } = require('../mongo/crud/users');
 const foxyEncryptionKey = process.env.FOXY_ENCRYPTION_KEY;
 const foxyStoreId = process.env.FOXY_STORE_ID;
 const foxyStoreDomain = process.env.FOXY_STORE_DOMAIN;
@@ -15,13 +15,15 @@ const validSignature = (headers, payload) => {
 
 const validRequest = (headers, body) => {
   if (!validSignature(headers, body)) {
-    console.log(('validSignature is TRUE!'))
+    console.log(('validSignature is TRUE!'));
     return false;
   }
   if (headers.method !== 'POST' || 
       headers['foxy-store-domain'] !== 'thwartme.foxycart.com' ||
       headers['foxy-store-id ']!== '98241'
   ) {
+    console.log('store domain in headers', headers['foxy-store-domain']);
+    console.log('store id in headers', headers['foxy-store-id ']);
     return false;
   }
   return true;
@@ -30,7 +32,7 @@ const validRequest = (headers, body) => {
 foxyRouter.post('/', async (req, res, next) => {
   try {
     if (validRequest(req.headers, req.body)) {
-      console.log(req.body)
+      console.log(req.body);
       res.status(200).json({ text: 'Success' });
     } else {
       res.status(403).json({ text: 'Not Authorized' });
