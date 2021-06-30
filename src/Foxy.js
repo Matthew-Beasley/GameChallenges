1ccc34bb39f9dac0eba753cfb544b74c4530e356
Matthew Beasley <matthewbeasley@Matthews-MacBook-Pro.local>
2021-06-29 22:21:13 -0700

    hmac broken, waiting for support

--- src/Foxy.js
+++ src/Foxy.js
@@ -9,9 +9,6 @@ import { userState, headerState, csrfState, keyState, tokenState } from './Recoi
 import { useCookies } from 'react-cookie';
 import  CryptoJS from 'crypto-js';
 
-/*var hash = CryptoJS.HmacSHA256("Message", "secret");
-  var hashInBase64 = CryptoJS.enc.Base64.stringify(hash);*/
-
 
 const Foxy = () => {
   const headers = useRecoilValue(headerState);
@@ -27,8 +24,6 @@ const Foxy = () => {
 
   useEffect(() => {
     axios.defaults.headers.post['X-CSRF-Token'] = cookies.CSRF_token;
-    //console.log('csrf token ', cookies.CSRF_token)
-    //console.log('token in useEffect ', cookies.token)
     if(cookies.token) {
       axios.post('/user/token', { token: cookies.token }, headers)
         .then(response => {
@@ -96,12 +91,31 @@ const Foxy = () => {
   };
 
   const hashedRef = (game, deck) => {
-    const concatThis = `${game.replace(/\s/g, '')}${deck.toString()}name${game} deck ${deck}price1.99quantity1`;
-    const hash = CryptoJS.HmacSHA256(concatThis, key);
-    const hashInBase64 = CryptoJS.enc.Base64.stringify(hash);
-    //console.log(`https://thwartme.foxycart.com/cart?code||${hashInBase64}${game.replace(/\s/g, '+')}+${deck}`)
-    return `https://thwartme.foxycart.com/cart?name||${hashInBase64}${game.replace(/\s/g, '+')}+deck+${deck}`;
-    
+    const code = `${game.replace(/\s/g, '')}${deck.toString()}`;
+    console.log('plain text code ', 'abc123');
+    const hashCode = CryptoJS.HmacSHA256('abc123', 'U794s2F9FxHjBB9z1PcwvpFjXbYWQYbxkT17gwfH44cFJRuRvM52WWxF5iSi');
+    const codeHex = CryptoJS.enc.Hex.stringify(hashCode);
+    console.log('hex hashed code ', codeHex)
+    //console.log('no enecoding ', hashCode)
+
+    //const signature = crypto.createHmac("sha256", key).update(query).digest("hex");
+    //console.log('hash ', signature)
+
+    const name = `${game} deck ${deck}`;
+    const hashName = CryptoJS.HmacSHA256(name, key);
+    const nameBase64 = CryptoJS.enc.Base64.stringify(hashName);
+
+    const hashPrice = CryptoJS.HmacSHA256('1.99', key);
+    const priceBase64 = CryptoJS.enc.Base64.stringify(hashPrice);  
+
+    const hashQuantity = CryptoJS.HmacSHA256('1', key);
+    const quantityBase64 = CryptoJS.enc.Base64.stringify(hashQuantity);
+    //return `https://thwartme.foxycart.com/cart?code=${code}||${codeHex}&name=${name.replace(/\s/g, '+')}||${nameBase64}&price=1.99||${priceBase64}&quantity=1.99||${quantityBase64}`;
+    //const hash = CryptoJS.HmacSHA256('abc123', key);
+    //const hashHex = CryptoJS.enc.Hex.stringify(hash)
+    //console.log(hashHex)
+    //return `https://thwartme.foxycart.com/cart?code=abc123||18f73b1224c91388b428cec6f1f54b15be2aee82234176b6005baf4fecb007f9&name=theproduct||17acff737eee90b3d51a174bc51532eb99cca849617d11ea98fee5ed153c3361&price=1.99||8172cb09d338c946605ca1a0959d1d9031444e928df45651995abf1630a8c705`
+    return null;
   };
 
   if(cookies.token) {
@@ -135,7 +149,7 @@ const Foxy = () => {
                         if(deck > 0 && user.decks.length > 0 && !user.decks.find(testDeck => testDeck.code === `${game}${deck}`)) {
                           return (
                             <li key={el}>
-                              {console.log(hashedRef(game, deck))}
+                              {/*console.log(hashedRef(game, deck))*/}
                               <a href={hashedRef(game, deck)}>Add {`${game} deck ${deck} $1.99`}</a>
                               {/*<a href={`https://thwartme.foxycart.com/cart?name=${game}${deck}&price=1.99&code=${game}${deck}`}>Add {`${game} deck ${deck} $1.99`}</a>*/}
                             </li>
