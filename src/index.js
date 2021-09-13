fb3c215b02584930a97601b57293be217d6c43f3
Matthew Beasley <matthewbeasley@Matthews-MacBook-Pro.local>
2021-09-13 09:05:47 -0700

    refactored create user styles

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import '../assets/styles/aboutus.css';
 import '../assets/styles/contactus.css';
 import '../assets/styles/game.css';
 import '../assets/styles/login.css';
+import '../assets/styles/createUser.css';
 
 const root = document.querySelector('#root');
 
