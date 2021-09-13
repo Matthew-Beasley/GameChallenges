6d0b4a5298d3bbaa320c4cf11c8514b1af618119
Matthew Beasley <matthewbeasley@Matthews-MacBook-Pro.local>
2021-09-13 14:15:45 -0700

    landing page responsive for phones

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
-import { csrfState, tokenState, emailKeyState } from './RecoilState';;
+import { csrfState, tokenState, emailKeyState } from './RecoilState';
 import { useCookies } from 'react-cookie';
 import CryptoJS from 'crypto-js';
 import axios from 'axios';
@@ -90,38 +90,64 @@ const LandingPage = () => {
   }, [csrf]);
 
   return (
-    <div id="landingpage">
-      <NavBar />
-      <div id="landingpage-wrapper">
-        <div id="landingpage-image">
-          <div id="signup" >
-            <div id="bubble">
-              <img src="../assets/images/SignupNow-small.png" />
+    <div>
+      <div id="landingpage">
+        <NavBar />
+        <div id="landingpage-wrapper">
+          <div id="landingpage-image">
+            <div id="signup" >
+              <div id="bubble">
+                <img src="../assets/images/SignupNow-small.png" />
+              </div>
+              <div id="signup-text" onClick={() => history.push('/login')}>
+                <div>Sign Up Now!</div>
+                <div>Play For Free!</div>
+              </div>
             </div>
-            <div id="signup-text" onClick={() => history.push('/login')}>
-              <div>Sign Up Now!</div>
-              <div>Play For Free!</div>
-            </div>
+            <img src="../assets/images/GameOn-small.png" />
+            {/*<div id="table"></div>*/}
           </div>
-          <img src="../assets/images/GameOn-small.png" />
-          {/*<div id="table"></div>*/}
-        </div>
-        <div id="landingpage-text">
-          <div id="thwartme-header">Thwart Me!</div>
-          <div className="landingpage-header" >Challenge your friends to ridiculous stunts in games you already have!</div>
-          <div className="landingpage-header" >Never play the objective again:</div>
-          <ol>
-            <li>Gather your gaming friends.</li>
-            <li>Pull out all those games that you loved but have played out.</li>
-            <li>Start a round of “Thwart Me!”</li>
-          </ol>
-          <p>
+          <div id="landingpage-text">
+            <div id="thwartme-header">Thwart Me!</div>
+            <div className="landingpage-header" >Challenge your friends to ridiculous stunts in games you already have!</div>
+            <div className="landingpage-header" >Never play the objective again:</div>
+            <ol>
+              <li>Gather your gaming friends.</li>
+              <li>Pull out all those games that you loved but have played out.</li>
+              <li>Start a round of “Thwart Me!”</li>
+            </ol>
+            <p>
           You’ll find new ways to love your games all over again, as you attempt crazy 
           challenges that have nothing to do with how the game was meant to be played.  
           Whether it’s an unconventional race or a treasure hunt, you’ll laugh your 
           way through an epic fail or<br/> ultimate victory.
-          </p>
-          <button onClick={() => history.push('/readmore')}>Read More</button>
+            </p>
+            <button onClick={() => history.push('/readmore')}>Read More</button>
+          </div>
+        </div>
+        <div id="landingpage-phone">
+          <img src="../assets/images/right-3.png" />
+          <div className="phone-bubble top-bubble" onClick={() => history.push('/login')}>
+            <div className="signup-text">Sign Up Now!</div> 
+            <div className="signup-text">Play For Free!</div>
+          </div>
+          <div className="phone-bubble">
+            <div id="thwartme-header">Thwart Me!</div>
+            <div className="landingpage-header" >Challenge your friends to ridiculous stunts in games you already have!</div>
+            <div className="landingpage-header" >Never play the objective again:</div>
+            <ol>
+              <li>Gather your gaming friends.</li>
+              <li>Pull out all those games that you loved but have played out.</li>
+              <li>Start a round of “Thwart Me!”</li>
+            </ol>
+            <p>
+            You’ll find new ways to love your games all over again, as you attempt crazy 
+            challenges that have nothing to do with how the game was meant to be played.  
+            Whether it’s an unconventional race or a treasure hunt, you’ll laugh your 
+            way through an epic fail or<br/> ultimate victory.
+            </p>
+            <button onClick={() => history.push('/readmore')}>Read More</button>
+          </div>
         </div>
       </div>
     </div>
