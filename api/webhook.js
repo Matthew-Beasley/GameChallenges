6aed7a6a52e1c99c4ad17d31f0a98596182f484e
Matthew Beasley <matthewbeasley@Matthews-MacBook-Pro.local>
2021-10-28 12:54:46 -0700

    fixed syntax error breaking build

--- api/webhook.js
+++ api/webhook.js
@@ -28,7 +28,7 @@ const validRequest = (request) => {
 
 webhook.post('/', async (req, res, next) => {
   try {
-    if (validRequest(req) {
+    if (validRequest(req)) {
       addTransaction(req.body);
       res.status(200).json({ text: 'Success' });
     } else {
