5bf513deea350708e3a51677f852d64c71322af8
Matthew Beasley <matthewbeasley@Matthews-MacBook-Pro.local>
2021-11-24 22:19:21 -0800

    changes to landing page, new background image with text

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -139,24 +139,8 @@ const LandingPage = () => {
                 <div>Play For Free!</div>
               </div>
             </div>
-            <img src="../assets/images/GameOn-small.png" />
+            <img src="../assets/images/GameOn-full.png" />
             {/*<div id="table"></div>*/}
-          </div>
-          <div id="landingpage-text">
-            <div id="thwartme-header">Thwart Me!</div>
-            <div className="landingpage-header" >Challenge your friends to ridiculous stunts in games you already have!</div>
-            <div className="landingpage-header" >Never play the objective again:</div>
-            <ol>
-              <li>Gather your gaming friends.</li>
-              <li>Pull out all those games that you loved but have played out.</li>
-              <li>Start a round of “Thwart Me!”</li>
-            </ol>
-            <p>
-          You’ll find new ways to love your games all over again, as you attempt crazy 
-          challenges that have nothing to do with how the game was meant to be played.  
-          Whether it’s an unconventional race or a treasure hunt, you’ll laugh your 
-          way through<br/> an epic fail or<br/> ultimate victory.
-            </p>
           </div>
         </div>
         <div id="landingpage-phone">
