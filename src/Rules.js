6f9d63d060a8816f8acce32278b6ebb4b2e192cf
Matthew Beasley <matthewbeasley@Matthews-MacBook-Pro.local>
2022-01-14 11:10:07 -0800

    fixed spelling error in rules

--- src/Rules.js
+++ src/Rules.js
@@ -14,7 +14,7 @@ const Rules = () => {
         <p>First, decide which device and user is going to run the game.  Everyone’s scores will be entered in the one place.</p> 
         <p>Start by creating a user and logging in.</p>
         <p>Next, to get the party started, you’ll need to choose a deck (or decks) to play.  The first deck is free.  After that, they’re $1.99/deck.  The list of available choices is always growing!</p>
-        <p>Once you’re in the game, enter each player’s name, and choose any filters to narrow down which of your decka you want to play.  Your first challenge will be displayed.  You’ll know whose turn it is because their avatar in the navigation bar will be elevated.  When the challenge is over, the player enters whether or not they were successful.  The score will appear on the scoreboard and then the next player’s avatar will jump up.</p>
+        <p>Once you’re in the game, enter each player’s name, and choose any filters to narrow down which of your decks you want to play.  Your first challenge will be displayed.  You’ll know whose turn it is because their avatar in the navigation bar will be elevated.  When the challenge is over, the player enters whether or not they were successful.  The score will appear on the scoreboard and then the next player’s avatar will jump up.</p>
       </div>
     </div>
   );
